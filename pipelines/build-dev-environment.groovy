172be85f5ad21b21263eaec9c9001e1d3bdd3da7
Vinicius S. Almeida <viniciussalmeida@gmail.com>
2023-08-03 22:02:20 -0300

    ci(pipeline): :wrench: Adjust GIT repository info
    Adjusting info of the current GIT repository used to checkout the pipeline as code files

--- pipelines/build-dev-environment.groovy
+++ pipelines/build-dev-environment.groovy
@@ -19,9 +19,9 @@ pipeline {
         stage('Checkout GIT repository') {
             steps {     
               script {
-                git branch: 'master',
-                credentialsId: '21f01d09-06da9cc35103',
-                url: 'git@mysecret-nonexistent-repo/jenkins.git'
+                git branch: 'develop',
+                // credentialsId: '21f01d09-06da9cc35103',
+                url: 'https://github.com/viniciussalmeida/jenkins-pipeline.git'
               }
             }
         }
